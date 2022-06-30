77ea8d21a17f85f0d09ccbc419fc68a06c20bb71
henriquevital00 <henriquevital1000@hotmail.com>
2022-06-30 17:27:51 -0300

    Section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -12,7 +12,7 @@ namespace API.Entities
         public DateTime DateOfBirth {get; set; }
         public DateTime Created {get; set; } = DateTime.Now;
         public DateTime LastActive {get; set; } = DateTime.Now;
-        public string KnowAs { get; set; }
+        public string KnownAs { get; set; }
         public string Gender { get; set; }
         public string Introduction { get; set; }
         public string LookingFor { get; set; }
@@ -20,5 +20,7 @@ namespace API.Entities
         public string City { get; set; }
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -20,7 +20,7 @@ export class MemberCardComponent implements OnInit {
 
   addLike(member: Member) {
     this.memberService.addLike(member.username).subscribe(() => {
-      this.toastr.success('You have liked ' + member.knownAs);
+      this.toastr.success('You have liked ' + member.username);
     })
   }
 
